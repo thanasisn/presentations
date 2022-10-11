92bcee8469a2cebfa1d2ec6a6e577abb8eb0c03c
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-10-11 21:17:05 +0300

    Commit 2022-10-11 21:17

--- IRS2020_global_trends/GHI_trends.R
+++ IRS2020_global_trends/GHI_trends.R
@@ -6,6 +6,7 @@
 #'   - Natsis Athanasios^[Laboratory of Atmospheric Physics, AUTH, natsisthanasis@gmail.com]
 #'   - Alkiviadis Bais^[Laboratory of Atmospheric Physics, AUTH.]
 #'   - Charikleia Meleti^[Laboratory of Atmospheric Physics, AUTH.]
+#'   - Kleareti Tourpali^[Laboratory of Atmospheric Physics, AUTH.]
 #' date: "`r format(Sys.time(), '%F')`"
 #'
 #' documentclass: article
@@ -59,7 +60,23 @@ knitr::opts_chunk$set(fig.align  = "center" )
 #'
 #' ## Abstract
 #'
+#' Measurements of downward shortwave solar irradiance are performed at Thessaloniki since 1993 with a Kipp & Zonen CM-21 pyranometer. Data are recorded as 1 min means and are quality controlled by adopting the procedure proposed by Long et al. (2008). The 26-year long time series is analysed in order to investigate long- and short-term changes caused by changes in atmospheric composition over the site. The overall increasing trend in daily solar irradiation under all-sky conditions of 0.33% per year reported for the period 1993–2012 (Bais et al., 2013) is reconfirmed, although a stronger trend is calculated for the last decade of the data series.
 #'
+#' An iterative optimization method has been employed to tune the clear-sky detection algorithm of Reno el al. (2016) to the local conditions, in order to identify the cloud-free cases. A few simple global radiation models have been tested and adjustment factors for each model have been determined which match best the measured values.
+#'
+#' Linear trends have been calculated for the entire period and different seasons for both clear- and all-sky conditions, in an attempt to attribute the long-term variability to variations in atmospheric water vapour and aerosols. Ancillary information from a collocated Cimel sun-photometer (since 2004), and a pyrheliometer (since 2016) are used to verify the effects of water vapor and aerosols on clear-sky irradiance. Finally, data under cloudy conditions are analysed to uncover potential effects from changes in cloudiness that have occurred over the period or record.
+
+#' References
+#'
+#'  Bais, A. F., T. Drosoglou, C. Meleti, K. Tourpali, and N. Kouremeti (2013), Changes in surface shortwave solar irradiance from 1993 to 2011 at Thessaloniki (Greece), International Journal of Climatology, 33(13), 2871–2876.
+#'
+#' Long, C. N., Y. Shi (2008), An Automated Quality Assessment and Control Algorithm for Surface Radiation Measurements, The Open Atmospheric Science Journal, 2, 23–37.
+#'
+#' Reno, M. J., C. W. Hansen (2016), Identification of periods of clear sky irradiance in time series of GHI measurements, Renewable Energy, 90, 520–531.
+#'
+#'
+#'
+
 
 
 
@@ -103,9 +120,3 @@ source("~/FUNCTIONS/R/trig_deg.R")
 #'
 #'
 
-
-#'
-#' **END**
-#+ include=T, echo=F
-tac <- Sys.time()
-cat(sprintf("\n%s %s@%s %s %f mins\n\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
